640e9c42d0e16e4f22ca6abd07f0519ef2c37b08
Manish Panda <pandamanish49@gmail.com>
2024-02-09 14:54:49 +0530

    book update

--- template/header.php
+++ template/header.php
@@ -29,40 +29,42 @@
 <body>
 
 <!-- Start Header/Navigation -->
-<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
+<div class="nav-container">
+    <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
-<div class="container">
-    <a class="navbar-brand" href="index">BookSell<span>.</span></a>
+    <div class="container">
+        <a class="navbar-brand" href="index">BookSell<span>.</span></a>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarsFurni">
-        <?php 
-        $front_page = basename($_SERVER['REQUEST_URI']);
-        // echo "<script> console.log('$front_page')</script>";
-        ?>
+        <div class="collapse navbar-collapse" id="navbarsFurni">
+            <?php 
+            $front_page = basename($_SERVER['REQUEST_URI']);
+            // echo "<script> console.log('$front_page')</script>";
+            ?>
 
-        <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0" id="custom-navbar-nav">
-            <li class="<?php echo ($front_page == "book" || $front_page == "index") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="index">Home</a></li>
-            <li class="<?php echo ($front_page == "about") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="about">About us</a></li>
-            <li class="<?php echo ($front_page == "shop") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="shop">Shop</a></li>
-            <li class="<?php echo ($front_page == "services") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="services">Services</a></li>
-            <!-- <li class="<?php echo ($front_page == "blog") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="blog">Blog</a></li> -->
-            <li class="<?php echo ($front_page == "contact") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="contact">Contact us</a></li>
-        </ul>
+            <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0" id="custom-navbar-nav">
+                <li class="<?php echo ($front_page == "book" || $front_page == "index") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="index">Home</a></li>
+                <li class="<?php echo ($front_page == "about") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="about">About us</a></li>
+                <li class="<?php echo ($front_page == "shop") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="shop">Shop</a></li>
+                <li class="<?php echo ($front_page == "services") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="services">Services</a></li>
+                <!-- <li class="<?php echo ($front_page == "blog") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="blog">Blog</a></li> -->
+                <li class="<?php echo ($front_page == "contact") ? 'head-menu active' : 'head-menu'; ?>"><a class="nav-link" href="contact">Contact us</a></li>
+            </ul>
 
-  
-
-        <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-            <li><a class="nav-link" href="#"><img src="images/user.svg"></a></li>
-            <li><a class="nav-link" href="cart"><img src="images/cart.svg"></a></li>
-        </ul>
-    </div>
-</div>
     
-</nav>
+
+            <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+                <li><a class="nav-link" href="#"><img src="images/user.svg"></a></li>
+                <li><a class="nav-link" href="cart"><img src="images/cart.svg"></a></li>
+            </ul>
+        </div>
+    </div>
+        
+    </nav>
+</div>
 <!-- End Header/Navigation -->
 
-<main>
+<main class="main-container">
